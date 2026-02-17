b7e22890f72739a101fd7962749ddb4878ae8906
Gregory-Isaac <gregoryisaac473@gmail.com git config --global user.name Gregory-Isaac>
2026-02-17 17:12:08 +0300

    first commit

---
+++ lesson6a.py
@@ -0,0 +1,16 @@
+#puthon module 
+#is a file that contains python definitions, statements and/or functions,statements and/functions
+ 
+
+def add()
+    
+    num1 =20
+    num2 =30
+    sum =num1 +num2
+    print("the answer is", sum)
+
+    def subtract()
+        x = 45
+        y = 30
+        difference x - y
+        print("")
---
+++ lesson6b.py
@@ -0,0 +1,8 @@
+# we can import the functions created in lesson6a module and use them on this file 
+
+# method 1
+import lesson6a
+
+# access the function inthe given module
+# to access the functions in the previous module 
+lesson6a.add( the answer is)
